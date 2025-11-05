772b38d952ff431cd07adda00daaa93f65db4b93
stheffanysantos <stheffany.sts@gmail.com>
2025-11-05 11:27:32 -0300

    alteração permitindo todos a utilizar a api

--- src/main/java/org/example/backendrh/config/CorsConfig.java
+++ src/main/java/org/example/backendrh/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://project-rh-web.vercel.app/")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
