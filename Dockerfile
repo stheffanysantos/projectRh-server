04f16d609e1e79e6cce45320b5856520b683cdc6
stheffanysantos <stheffany.sts@gmail.com>
2025-11-05 10:05:18 -0300

    alteração no dockerfile na url do front no cors e o build.gradle

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM eclipse-temurin:17-jdk-alpine
+
+WORKDIR /app
+COPY . .
+RUN ./gradlew build
 VOLUME /tmp
 COPY build/libs/*.jar app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
+ENTRYPOINT ["java", "-jar", "/app/app.jar"]
--- src/main/java/org/example/backendrh/config/CorsConfig.java
+++ src/main/java/org/example/backendrh/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://project-rh-web.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
