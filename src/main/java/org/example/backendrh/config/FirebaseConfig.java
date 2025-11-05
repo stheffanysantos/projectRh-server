744dd77815bab6c9ae201b2d1bb3cacffd865649
stheffanysantos <stheffany.sts@gmail.com>
2025-11-05 09:33:41 -0300

    alteração nas configurações do firebase

--- src/main/java/org/example/backendrh/config/FirebaseConfig.java
+++ src/main/java/org/example/backendrh/config/FirebaseConfig.java
@@ -5,19 +5,24 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-
-
-import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
     @Bean
     public FirebaseApp firebaseApp() throws Exception {
-        InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json");
+        // Recupera o conteúdo do JSON do Firebase de uma variável de ambiente
+        String firebaseConfig = System.getenv("FIREBASE_CONFIG");
+        if (firebaseConfig == null) {
+            throw new IllegalStateException("FIREBASE_CONFIG environment variable not set!");
+        }
+        InputStream serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes(StandardCharsets.UTF_8));
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://SEU-PROJETO.firebaseio.com/")
+                // substitua o valor abaixo pelo URL real do seu projeto
+                .setDatabaseUrl("https://sistemarh-601cf.firebaseio.com/")
                 .build();
         return FirebaseApp.initializeApp(options);
     }
